fbd5f19bef0d87d5547a74e50020d3383e9287f0
Jacob Gardner <jgardner@nerdery.com>
2017-08-18 00:48:29 -0500

    Initialize guppy

--- tasks/gitHooks.js
+++ tasks/gitHooks.js
@@ -1,5 +1,7 @@
 const gulp = require('gulp');
 const sequence = require('gulp-sequence');
 
+const guppy = require('git-guppy')(gulp);
+
 gulp.task('pre-commit', ['prettier']);
 gulp.task('pre-push', ['deploy']);
