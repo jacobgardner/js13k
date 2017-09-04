bdc19a6dab85ee0c3b97eb11b173fb82319a6263
Jacob Gardner <jacob.v.gardner@gmail.com>
2017-09-04 01:02:50 -0500

    Some better codez

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { initGL } from './gl';
 import Renderer from './renderer';
 
 import Maze from './maze';
+import {Node} from './grid';
 import { SIZE_X, SIZE_Y, PLAYER_SPEED } from './config';
 import Player from './player';
 
@@ -36,22 +37,49 @@ interface Map<T> {
 const downMap: Map<number> =  {}
 
 function processInput() {
+    let [x, y] = [player.x, player.y];
     if (downMap.w) {
-        player.y -= PLAYER_SPEED;
+        y -= PLAYER_SPEED;
     }
 
     if (downMap.s) {
-        player.y += PLAYER_SPEED;
+        y += PLAYER_SPEED;
     }
 
     if (downMap.a) {
-        player.x -= PLAYER_SPEED;
+        x -= PLAYER_SPEED;
     }
 
     if (downMap.d) {
-        player.x += PLAYER_SPEED;
+        x += PLAYER_SPEED;
     }
 
+    const px = player.x;
+    const py = player.y;
+    const current = maze.grid.get(Math.floor(px), Math.floor(py)) as Node;
+    const buffer = 0.3;
+    if (Math.floor(x) !== Math.floor(px)) {
+        const node = maze.grid.get(Math.floor(x), Math.floor(py)) as Node;
+        if (current.children.indexOf(node) !== -1) {
+            player.x = x;
+        }
+    // } else if (Math.floor(x + buffer) > Math.floor(px)) {
+    //     const node = maze.grid.get(Math.floor(x + buffer), Math.floor(py)) as Node;
+    //     if (current.children.indexOf(node) !== -1) {
+    //         player.x = x;
+    //     }
+    } else {
+        player.x = x;
+    }
+
+    if (Math.floor(y) !== Math.floor(py)) {
+        const node = maze.grid.get(Math.floor(px), Math.floor(y)) as Node;
+        if (current.children.indexOf(node) !== -1) {
+            player.y = y;
+        }
+    } else {
+        player.y = y;
+    }
 }
 
 onkeydown = (evt) => {
--- src/player.ts
+++ src/player.ts
@@ -3,6 +3,8 @@ import Maze from './maze';
 import { vertex, playerFrag } from './shaders/shaders';
 import { setMatrix } from './lib';
 
+const PLAYER_SCALE = 0.4;
+
 export default class Player {
     x: number;
     y: number;
@@ -10,8 +12,8 @@ export default class Player {
 
     constructor(public renderer: Renderer, public maze: Maze) {
         [this.x, this.y] = [
-            maze.start.position[0] + 0.25,
-            maze.start.position[1] + 0.25
+            maze.start.position[0] + 0.5,
+            maze.start.position[1] + 0.5
         ];
 
         this.program = new Program(renderer, vertex, playerFrag);
@@ -25,7 +27,7 @@ export default class Player {
         gl.bindBuffer(gl.ARRAY_BUFFER, this.renderer.squareBuffer);
         gl.vertexAttribPointer(this.program.vertPos, 2, gl.FLOAT, false, 0, 0);
 
-        this.renderer.modelMat = setMatrix(this.x, this.y, 0.5);
+        this.renderer.modelMat = setMatrix(this.x - PLAYER_SCALE / 2, this.y - PLAYER_SCALE / 2, PLAYER_SCALE);
         this.renderer.setMatrices();
         gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
 
